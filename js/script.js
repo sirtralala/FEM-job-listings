98fef13fd54a81f4f7cd1abcee42ad0956ba5e18
sirtralala <thomas.doerflinger@gmx.net>
2020-09-05 12:58:11 +0200

    filter algorithm added, final touches added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var filterList = [];
+var items = document.querySelectorAll('.item');
 
 displayFilterInFilterBar = filter => {
     let filterBar = document.querySelector('.filter__labels');
@@ -24,40 +25,10 @@ toggleFilterBar = () => {
     filterList.length ? filterBarStyle.display = 'flex' : filterBarStyle.display = 'none';
 }
 
-displayResult = filter => {
-    filter = filter.toLowerCase();
-    let items = document.querySelectorAll('.item');
-    
+toggleAllItems = style => {
     items.forEach( item => {
-        if (item.dataset.role.includes(filter)) {
-            item.style.display = 'flex';
-        }
-        else if (item.dataset.level.includes(filter)) {
-            item.style.display = 'flex';
-        }
-        else if (item.dataset.languages && item.dataset.languages.includes(filter)) {
-            item.style.display = 'flex';
-        }
-        else if (item.dataset.tools && item.dataset.tools.includes(filter)) {
-            item.style.display = 'flex';
-        }
-        else {
-            item.style.display = 'none';
-        }
-    });
-}
-
-addFilter = filter => {
-    addFilterToList(filter);
-    toggleFilterBar();
-    displayResult(filter);
-}
-
-displayAllItems = () => {
-    let allItems = document.querySelectorAll('.item');
-    for (let i = 0; i < allItems.length; i++) {
-        allItems[i].style.display = 'flex';
-    }
+        item.style.display = style;
+    })
 }
 
 emptyFilterList = () => {
@@ -65,19 +36,66 @@ emptyFilterList = () => {
     document.querySelector('.filter').innerHTML = `<div class="filter__labels">&nbsp;</div>
                                                    <div class="filter__clear" onclick="emptyFilterList()">Clear</div>`;
     toggleFilterBar();
-    displayAllItems();
+    toggleAllItems('flex');
+}
+
+pushFilteredItems = (arr, filter) => {
+    let temp = [];
+
+    arr.forEach( item => {
+        if (item.dataset.role.includes(filter)) {
+            temp.push(item);
+        }
+        else if (item.dataset.level.includes(filter)) {
+            temp.push(item);
+        }
+        else if (item.dataset.languages && item.dataset.languages.includes(filter)) {
+            temp.push(item);
+        }
+        else if (item.dataset.tools && item.dataset.tools.includes(filter)) {
+            temp.push(item);
+        }
+    });
+    return temp;
+}
+
+filterResult = () => {
+    let result = [];
+
+    for (let i = 0; i < filterList.length; i++) {
+        if (i == 0) {
+            result = pushFilteredItems(items, filterList[i].toLowerCase());
+        }
+        result = pushFilteredItems(result, filterList[i].toLowerCase());
+    }
+    return result;
+}
+
+displayFilteredItems = result => {
+    toggleAllItems('none');
+
+    result.forEach( item => {
+        item.style.display = 'flex';
+    });
+}
+
+addFilter = filter => {
+    addFilterToList(filter);
+    toggleFilterBar();
+    displayFilteredItems(filterResult());
 }
 
 removeItemFromList = filter => {
     let index = filterList.indexOf(filter);
+
     if (filterList.length > 1) {
         filterList.splice(index, 1);
-        displayResult(filterList[filterList.length - 1]);
+        displayFilteredItems(filterResult());
     }
     else {
         filterList.splice(index, 1);
         toggleFilterBar();
-        displayAllItems();
+        toggleAllItems('flex');
     }
 }
 
